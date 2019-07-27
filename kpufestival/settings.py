d02425a7b86f43683ce5da2ec4ce2c071a887e25
ed502 <49014196+ed502@users.noreply.github.com>
2019-07-28 01:33:53 +0900

    베이스 만드는중

--- kpufestival/settings.py
+++ kpufestival/settings.py
@@ -121,3 +121,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'account', 'static'),
+    os.path.join(BASE_DIR, 'board', 'static'),
+    os.path.join(BASE_DIR, 'info', 'static'),
+    os.path.join(BASE_DIR, 'kpufestival', 'static')
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
