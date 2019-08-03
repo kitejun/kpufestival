d02a16c5c0f0841fc269bbd6c085d23438af46fd
YangSSo51 <48993188+YangSSo51@users.noreply.github.com>
2019-08-04 01:44:29 +0900

    base templates

--- kpufestival/urls.py
+++ kpufestival/urls.py
@@ -15,7 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+import info.views
+import account.views
+import board.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',info.views.home,name="home"),
+    path('login/',account.views.login,name="login"),
+    path('signup/',account.views.signup,name="signup"),
+    path('board/',board.views.board,name="board"),
+    path('introduce/',board.views.introduce,name="introduce"),
+
 ]
