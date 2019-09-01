0dae1a7472a3ec75afe63ad552b114a5151cb40c
이세민 <sky74zkr@gmail.com>
2019-09-01 14:17:55 +0900

    Revert "Revert "Revert "Merge branch 'master' into lsm"""
    This reverts commit 8618b816f92622e9f32f168a8d820fcae5ae50f7.

--- board/views.py
+++ board/views.py
@@ -90,9 +90,3 @@ def introduce(request):
 
 def intro_detail(request):
     return render(request,'intro_detail.html')
-
-# introduce 기능
-# introduce-학과명,좋아요개수,위치,소개글 표시/모달에서 지도 표시
-# intro_detail
-# -학과명,좋아요 개수,싫어요 개수,조회수,태그,소개글,학과 아이콘 표시/모달에서 지도 표시
-# -카톡 공유하기 기능(학과명,사진 공유 혹은 링크)#
