f15f09ff860bcc6c2d15ff8a9474e0480bbe9af3
YangSSo51 <48993188+YangSSo51@users.noreply.github.com>
2019-09-07 01:45:07 +0900

    WSGIPath 오류

--- kpufestival/settings.py
+++ kpufestival/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&
 DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 #DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [' django-env.pws5ppwp4q.us-west-2.elasticbeanstalk.com']
 
 
 # Application definition
