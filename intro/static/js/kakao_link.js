0af20d72d3225c9c75cea5a02e05376ea8f2421e
Mox <kimyj7756@gmail.com>
2019-09-18 23:46:23 +0900

    분실물 수정 삭제 추가

--- intro/static/js/kakao_link.js
+++ intro/static/js/kakao_link.js
@@ -1,15 +1,1 @@
 Kakao.init('5f2d23cd2c499b2da83462fb37ae738e');
-  function sendLink() {
-    Kakao.Link.sendDefault({
-      objectType: 'feed',
-      content: {
-        title: '학과: {{intro_detail.dename}}',
-        description: '소개: {{ intro_detail.tag }}',
-        imageUrl: 'http://kpufestival.waytech.kr/{{ intro_detail.img1.url }}/',
-        link: {
-          mobileWebUrl: 'http://kpufestival.waytech.kr/intro/intro_detail/{{ intro_detail.id }}/',
-          webUrl: 'http://kpufestival.waytech.kr/intro/intro_detail/{{ intro_detail.id }}/',
-        }
-      },
-    });
-  }
